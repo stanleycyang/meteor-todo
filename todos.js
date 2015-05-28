ec0b1a5db359bb1955160d6f96e2e24580c6238f
syang019 <stanleyang13@gmail.com>
2015-05-27 19:51:19 -0700

    Initialized Repo

---
+++ todos.js
@@ -0,0 +1,62 @@
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    tasks: function(){
+      if(Session.get('hideCompleted')){
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      }else{
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function(){
+      return Session.get('hideCompleted');
+    },
+    incompleteCount: function(){
+      return Tasks.find({checked: {$ne: true}}).count();
+    }
+  });
+
+  Template.body.events({
+    "change .hide-completed input": function(event){
+      Session.set('hideCompleted', event.target.checked);
+    },
+    // When a user submits a form
+    "submit .new-task": function(event){
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date(), // current time
+        owner: Meteor.userId(),
+        username: Meteor.user().username
+      });
+
+      event.target.text.value = "";
+
+      return false;
+
+    }
+  });
+
+  Template.task.events({
+    'click .toggle-checked': function(){
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+
+    'click .delete': function(){
+      Tasks.remove(this._id);
+    }
+  });
+
+  Accounts.ui.config({
+      passwordSignupFields: "USERNAME_ONLY"
+  });
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
